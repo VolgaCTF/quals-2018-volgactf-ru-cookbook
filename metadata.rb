a7958d7a8b612f22786e20f89a7d84b045424714
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2018-08-05 20:05:59 +0400

    Make us of updated tls cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'quals-2018-volgactf-ru'
 description 'Installs and configures quals.2018.volgactf.ru'
-version '1.1.0'
+version '1.1.1'
 
 recipe 'quals-2018-volgactf-ru', 'Installs and configures quals.2018.volgactf.ru'
 depends 'nodejs'
 depends 'nginx'
-depends 'tls', '~> 3.0.0'
+depends 'tls', '~> 3.0.3'
 depends 'instance', '~> 2.0.0'
